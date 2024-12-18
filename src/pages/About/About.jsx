eb6b4e1e30dd4c98dc998c56ecd4e63fab57d625
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2024-12-18 17:35:47 +0200

    add pages

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>О нас</h1>
+    <p>Мы создаем современные React-приложения.</p>
+  </div>
+);
+
+export default About;
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h1>404 - Страница не найдена</h1>
+    <p>К сожалению, запрашиваемая страница не существует.</p>
+  </div>
+);
+
+export default NotFound;
