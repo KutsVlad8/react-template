e282cd7b949e204d2bcfd3ff044f193664fc4eeb
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2024-12-18 17:36:09 +0200

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home>Home</Home>} />
+          <Route path="/about" element={<About>About</About>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
